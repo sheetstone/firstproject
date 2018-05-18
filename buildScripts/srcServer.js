c1de88dae15984b0a9625db978f02eb3b1ea7ef9
PSG Laptop <Owner@psgs-mbp.acsrr.com>
2018-05-18 12:39:35 -0500

    update

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -8,6 +8,7 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
+/* eslint-disable no-console */
 app.use(require('webpack-dev-middleware')(compiler,{
   noInfo:true,
   publicPath:config.output.publicPath
@@ -16,6 +17,14 @@ app.use(require('webpack-dev-middleware')(compiler,{
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
+
+app.get('/users',function(req,res){
+  res.json([
+    {"id":1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id":2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@gmail.com"},
+    {"id":3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@gmail.com"}
+  ])
+})
 
 app.listen(port,function(err){
   if(err){
--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 /* */
 import './styles/main.css';
-import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`);
+import { getUsers } from './api/userApi';
+
+// import numeral from 'numeral';
+
+// const courseValue = numeral(1000).format('$0,0.00');
+
+// Poluate table of users
+getUsers().then(result =>{
+  let usersBody = "";
+
+  result.forEach(user =>{
+    usersBody+=`<tr>
+    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>`
+  })
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
